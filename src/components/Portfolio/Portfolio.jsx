cc7467fc64b947e3d06eb6d4350b9548373dcf5a
padermo <fabioaeh96@gmail.com>
2022-12-17 18:03:44 -0500

    creando estilos para los proyectos

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import Proyect from './Proyect'
+import galleryLight from '../../assets/images/gallerylightbox.png'
 
 function Portfolio() {
+  const data = [
+    {
+      id: 1,
+      name: "Gallery Light Box",
+      img: galleryLight,
+      github: "https://github.com/padermo/GalleryLightBox",
+      demo: "https://gallery-light-box.vercel.app/"
+    },
+  ];
+
+  const [viewProyect, setViewProyect] = useState();
+
+  const onLoad = () => {
+    setViewProyect(
+      data.map(e => (
+        <Proyect name={e.name} img={e.img} github={e.github} demo={e.demo}/>
+      ))
+    )
+  }
+
+  useEffect(()=> {
+    onLoad();
+  },[]);
+
   return (
     <section className='portfolio' id='portfolio'>
-
+      <div className="portfolio__content container">
+        <h2 className="title title__margin">Portfolio</h2>
+        <div className="portfolio__info">
+          {viewProyect}
+        </div>
+      </div>
     </section>
   )
 }
