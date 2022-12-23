fe9353de923dfc5843cc18ef4679e6f03836f951
padermo <fabioaeh96@gmail.com>
2022-12-22 19:32:50 -0500

    actualizando componente cv en el archivo de estilos

--- src/components/CV/Cv.jsx
+++ src/components/CV/Cv.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Cv() {
   return (
     <div className='cv'>
-      <a href="https://drive.google.com/file/d/11m2LlJj1x2_vGrx9gSnkZjYpWjH13X7N/view?usp=share_link" target="_blank" className='cv__link cv__link--opacity'><span className='cv__text'>Descargar CV</span></a>
+      <a href="https://drive.google.com/file/d/11m2LlJj1x2_vGrx9gSnkZjYpWjH13X7N/view?usp=share_link" target="_blank" className='cv__link cv__link--opacity'><span className='cv__text'></span></a>
     </div>
   )
 }
