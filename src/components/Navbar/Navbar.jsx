c04ccd8c77478e0223598591126a9d728d08f1a9
padermo <fabioaeh96@gmail.com>
2022-12-27 17:45:50 -0500

    mejorando el navbar y algunos mq

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
 import logo from '../../assets/icons/logo.svg'
+import menu from '../../assets/icons/bx-menu.svg'
 
 function Navbar() {
   return (
     <nav className='nav'>
-      <figure className='nav__picture'>
-        <img src={logo} alt="Img icon" className='nav__img' />
-      </figure>
+      <div className="nav__content">
+        <figure className='nav__picture'>
+          <img src={logo} alt="Img icon" className='nav__img' />
+        </figure>
 
-      <div className='nav__links' >
-        <a href="#home" className='nav__link'>Home</a>
-        <a href="#about" className='nav__link'>About</a>
-        <a href="#skills" className='nav__link'>Skills</a>
-        <a href="#servicios" className='nav__link'>Services</a>
-        <a href="#portfolio" className='nav__link'>Portfolio</a>
-        <a href="#contact" className='nav__link'>Contact</a>
+        <div className='nav__links' >
+          <a href="#home" className='nav__link'>Home</a>
+          <a href="#about" className='nav__link'>About</a>
+          <a href="#skills" className='nav__link'>Skills</a>
+          <a href="#servicios" className='nav__link'>Services</a>
+          <a href="#portfolio" className='nav__link'>Portfolio</a>
+          <a href="#contact" className='nav__link'>Contact</a>
+        </div>
       </div>
     </nav>
   )
