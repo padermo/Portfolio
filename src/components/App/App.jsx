391bf70969d7189edf54295c820567e5e13e7c21
padermo <fabioaeh96@gmail.com>
2023-03-08 15:16:57 -0500

    agregando sass al proyecto y eliminando estilos normales

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import '../../style/normalize.css'
-import '../../style/style.css'
 import Header from '../Header/Header'
 import About from '../About/About'
 import Skills from '../Skills/Skills'
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './components/App/App';
+import './style/normalize.css'
+import './style/style.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
