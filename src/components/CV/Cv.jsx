40257a7e3ecb549050e00bbc78307c3bf849b25f
padermo <fabioaeh96@gmail.com>
2023-03-08 18:19:04 -0500

    cv terminada

--- src/components/CV/Cv.jsx
+++ src/components/CV/Cv.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Cv() {
   return (
-    <div className='cv'>
-      <a href="https://drive.google.com/file/d/1jC-JRL5fMbjehoca45WZQvfrJEO-1bFs/view?usp=share_link" target="_blank" className='cv__link cv__link--opacity'><span className='cv__text'></span></a>
-    </div>
+    <>
+      <a href="https://drive.google.com/file/d/1jC-JRL5fMbjehoca45WZQvfrJEO-1bFs/view?usp=share_link" target="_blank" rel='noreferrer' className='cv'>CV</a>
+    </>
   )
 }
 
