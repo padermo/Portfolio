9e02643f08de44888c5df67e0494a72d8493eb52
padermo <fabioaeh96@gmail.com>
2023-03-21 11:45:52 -0500

    cambiando iconos y link cv

--- src/components/CV/Cv.jsx
+++ src/components/CV/Cv.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Cv() {
   return (
     <>
-      <a href="https://drive.google.com/file/d/1jC-JRL5fMbjehoca45WZQvfrJEO-1bFs/view?usp=share_link" target="_blank" rel='noreferrer' className='cv'>CV</a>
+      <a href="https://drive.google.com/file/d/1JiMrURaVNL4tB6C8-npcqUcunxSjPjJS/view?usp=share_link" target="_blank" rel='noreferrer' className='cv'>CV</a>
     </>
   )
 }
